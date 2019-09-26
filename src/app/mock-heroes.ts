9f50b6b893fbdd00896134345560343c7fdec765
Nicholas Chinsen <nick_irongate@hotmail.com>
2019-09-26 18:20:01 -0400

    test

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Sr. Nick' },
+  { id: 12, name: 'Testo' },
+  { id: 13, name: 'Pickle Man' },
+  { id: 14, name: 'Doosts' },
+  { id: 15, name: 'Typhoon' },
+  { id: 16, name: 'Ghibli' },
+  { id: 17, name: '911' },
+  { id: 18, name: 'Stellaris' },
+  { id: 19, name: 'Diamonoid' },
+  { id: 20, name: 'Dr. Rheeee' }
+];
